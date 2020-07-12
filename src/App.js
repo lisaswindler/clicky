a58e0c646370f22b335e8d78d595b4b9146479b5
lisacampbell <59990057+lisaswindler@users.noreply.github.com>
2020-07-11 23:52:51 -0600

    re-adding files

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import AnimalCard from "./components/AnimalCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import Nav from "./components/Nav";
+import animals from "./animals.json";
+
+class App extends Component {
+  state = {
+    animals,
+    correctGuesses: 0,
+    bestScore: 0,
+    clickMessage: "Click away! But don't click on the same animal twice."
+  };
+
+  clickOnAnimal = id => {
+   console.log("clicked");
+   this.shuffleAnimals();
+  };
+
+  shuffleAnimals = () => {
+    let cards = this.state.animals;
+    for (let i = 0; i < cards.length - 1; i++) {
+      const j = i + Math.floor(Math.random() * (cards.length - i));
+
+      const temp = cards[j];
+      cards[j] = cards[i];
+      cards[i] = temp;
+    }
+    this.setState({matches: cards});
+  };
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
+  render() {
+    return (
+      <Wrapper>
+          <Nav></Nav>
+        <Title>Wild & Clicky</Title>
+        {this.state.animals.map(animal => (
+          <AnimalCard
+            clickOnAnimal={this.clickOnAnimal}
+            id={animal.id}
+            key={animal.id}
+            name={animal.name}
+            image={animal.image}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Nav/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function NavTabs() {
+  return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        Click on the images to play!
+      </li>
+    </ul>
+  );
+}
+
+export default NavTabs;
