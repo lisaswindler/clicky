54973f382bf4572aa73d53eb9e732fed6fc399a9
lisacampbell <59990057+lisaswindler@users.noreply.github.com>
2020-07-11 22:49:20 -0600

    adding initial files

---
+++ clicky/src/components/Nav/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./style.css";
+
+function NavTabs() {
+  return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        
+      </li>
+      <li className="nav-item">
+       
+        
+      </li>
+      <li className="nav-item">
+        
+      </li>
+      <li className="nav-item">
+       
+      </li>
+    </ul>
+  );
+}
+
+export default NavTabs;
